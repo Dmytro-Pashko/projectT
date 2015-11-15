237d20037261cf8c7440390d6902e8af70d0d9cc
Goodvin1709 <goodvin1709@gmail.com>
2015-11-15 17:42:43 +0200

    OOP_3

--- core/src/com/mytrex/game/World.java
+++ core/src/com/mytrex/game/World.java
@@ -6,26 +6,23 @@ import com.mytrex.game.models.Player;
 /**
  * Created by Goodvin1709 on 10.11.2015.
  */
-public class World
-{
+public class World {
     private Player player;
 
     private int Width; //max world x.
     private int Height; //max world y.
 
 
-    public World()
-    {
+    public World() {
         player = new Player();
     }
 
-    public Player getPlayer()
-    {
+    public Player getPlayer() {
         return player;
     }
+
     //world render.
-    public void draw(SpriteBatch batch)
-    {
+    public void draw(SpriteBatch batch) {
         player.getSprite().draw(batch);
     }
 }
