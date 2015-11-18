0d55edc8b095797df4437e4243e0f836e70a6712
goodvin1709 <goodvin1709@gmail.com>
2015-11-18 23:15:19 +0200

    Update Player.java

--- core/src/com/mytrex/game/models/Player.java
+++ core/src/com/mytrex/game/models/Player.java
@@ -2,8 +2,6 @@ package com.mytrex.game.models;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 
@@ -12,62 +10,43 @@ import com.badlogic.gdx.physics.box2d.Body;
  */
 public class Player
 {
-    TextureRegion texture = new TextureRegion(new Texture("core/assets/wall.jpg"));
-    private Vector2 position;
-    private Vector2 velocity;
-    private Vector2 acceleraion;
-    float rotation;
-    int width;
-    int height;
-    SpriteBatch batch = new SpriteBatch();
+    private Body body;
+    private Sprite sprite;
+    private Boolean leftMove,rightMove,jump;
 
-    public Player(float x, float y, int width, int height){
-        this.width = width;
-        this.height = height;
-        position = new Vector2(x, y);
-        velocity = new Vector2(0, 0);
-        acceleraion = new Vector2(0, 460);
+    public Player(Body body)
+    {
+        this.body = body;
+        leftMove = false;
+        rightMove = false;
+        jump=false;
     }
 
-    public void draw(){
-        batch.begin();
-        batch.draw(texture, position.x, position.y, width, height);
-        batch.end();
+    public Boolean getJump() {
+        return jump;
     }
 
-    public void update(float delta){
-        velocity.add(acceleraion.cpy().scl(delta));
-
-        if (velocity.y > 200) {
-            velocity.y = 200;
-        }
-
-        position.add(velocity.cpy().scl(delta));
-
+    public void setJump(Boolean jump) {
+        this.jump = jump;
     }
 
-    public void onClick() {
-        velocity.y = -140;
+    public Body getBody() {
+        return body;
     }
 
-    public float getX() {
-        return position.x;
+    public void setLeftMove(Boolean leftMove) {
+        this.leftMove = leftMove;
     }
 
-    public float getY() {
-        return position.y;
+    public void setRightMove(Boolean rightMove) {
+        this.rightMove = rightMove;
     }
 
-    public float getWidth() {
-        return width;
+    public Boolean getLeftMove() {
+        return leftMove;
     }
 
-    public float getHeight() {
-        return height;
+    public Boolean getRightMove() {
+        return rightMove;
     }
-
-    public float getRotation() {
-        return rotation;
-    }
-
 }
