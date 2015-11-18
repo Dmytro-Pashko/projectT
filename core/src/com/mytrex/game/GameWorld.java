8f656fd29c09a4b08929951412445b0e3d63a332
goodvin1709 <goodvin1709@gmail.com>
2015-11-18 23:14:00 +0200

    Update GameWorld.java

--- core/src/com/mytrex/game/GameWorld.java
+++ core/src/com/mytrex/game/GameWorld.java
@@ -1,21 +1,21 @@
 package com.mytrex.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.mytrex.game.Tools.BodyEditorLoader;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.mytrex.game.models.GroundBlock;
 import com.mytrex.game.models.Player;
 
+import java.util.ArrayList;
 
 
 public class GameWorld
 {
     public Player player;
     private World world;
-    private Rectangle rectangle = new Rectangle(3, 7.5f ,0.5f , 0.5f);
-
+    public ArrayList<Object> list = new ArrayList<>();
 
     public World getWorld()
     {
@@ -24,34 +24,21 @@ public class GameWorld
 
     public GameWorld()
     {
-        player = new Player(1, 1, 1, 1);
         world = new World(new Vector2(0, -9.8f), true);
         for (int i = 0; i <10 ; i++)
         {
-            createGroundBlocks((float)i,8.0f);
+            list.add(new GroundBlock(createGroundBlocks(i, 0.0f)));
         }
-
+        player = new Player(initPlayer(2,2));
+        list.add(player);
     }
 
-
-    public Rectangle getRect(){
-        return rectangle;
-    }
-    public Player getPlayer(){
+    public Player getPlayer()
+    {
         return player;
     }
 
-
-    public void update(float delta){
-        Gdx.app.log("GameWorld", "update");
-        rectangle.x+= 0.2;
-        if (rectangle.x > 50){
-            rectangle.x = 0;
-        }
-    }
-
-
-    public void createGroundBlocks(float x,float y)
+    public Body createGroundBlocks(float x,float y)
     {
         BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("core/assets/ground.json"));
         BodyDef def = new BodyDef();
@@ -59,6 +46,27 @@ public class GameWorld
         Body body = world.createBody(def);
         loader.attachFixture(body,"wall",new FixtureDef(),1.0f);
         body.setTransform(x, y, 0);
-
+        return body;
+    }
+    public Body initPlayer(float x, float y){
+        BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("core/assets/box.json"));
+        BodyDef def = new BodyDef();
+        def.type = BodyType.DynamicBody;
+        Body body = world.createBody(def);
+        loader.attachFixture(body,"box",new FixtureDef(),1.0f);
+        body.setTransform(x, y,0);
+        return body;
+    }
+    public void update()
+    {
+        //System.out.println("Player X="+player.getBody().getPosition().x+" Player Y="+player.getBody().getPosition().y);
+        if (player.getLeftMove())
+        {
+            player.getBody().setTransform(player.getBody().getPosition().x-0.1f,player.getBody().getPosition().y,0);
+        }
+        if (player.getRightMove())
+        {
+            player.getBody().setTransform(player.getBody().getPosition().x+0.1f,player.getBody().getPosition().y,0);
+        }
     }
 }
