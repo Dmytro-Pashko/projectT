6940e73d01a0c301f94c83f2464956c4bda50715
goodvin1709 <goodvin1709@gmail.com>
2015-11-18 23:14:44 +0200

    Update GameScreen.java

--- core/src/com/mytrex/game/scene/GameScreen.java
+++ core/src/com/mytrex/game/scene/GameScreen.java
@@ -5,72 +5,49 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
 import com.mytrex.game.GameWorld;
-import com.mytrex.game.Tools.GameRenderer;
-import com.mytrex.game.helpers.InputHandler;
+import com.mytrex.game.PlayerInputProcessor;
 
-/**
- * Created by Goodvin1709 on 16.11.2015.
- */
 public class GameScreen implements Screen
 {
     private Game game;
     private Box2DDebugRenderer debuger;
-    private World world;
-    private GameRenderer renderer;
     private GameWorld gameWorld;
+    private OrthographicCamera camera;
 
     public GameScreen(Game game)
     {
-        debuger = new Box2DDebugRenderer();
-        debuger.setDrawVelocities(true);
-        debuger.setDrawContacts(true);
-        world = new GameWorld().getWorld();
         this.game = game;
         gameWorld = new GameWorld();
-        renderer = new GameRenderer(gameWorld);
-        Gdx.input.setInputProcessor(new InputHandler(gameWorld.getPlayer()));
-    }
-
-    @Override
-    public void show() {
-
+        debuger = new Box2DDebugRenderer();
+        Gdx.input.setInputProcessor(new PlayerInputProcessor(gameWorld));
+        camera = new OrthographicCamera(10,10);
+        camera.position.set(5,5,0);
     }
 
     @Override
     public void render(float delta)
     {
-        world.step(delta,5,5);
+        gameWorld.update();
+        camera.update();
+        gameWorld.getWorld().step(delta, 5, 5);
         Gdx.gl20.glClearColor(0,0,0,1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-        gameWorld.update(delta);
-        renderer.render();
+        debuger.render(gameWorld.getWorld(),camera.combined);
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
-
+    public void resize(int width, int height) {}
     @Override
-    public void pause() {
-
-    }
-
+    public void pause() {}
     @Override
-    public void resume() {
-
-    }
-
+    public void resume() {}
     @Override
-    public void hide() {
-
-    }
-
+    public void hide() {}
     @Override
-    public void dispose() {
-    }
+    public void dispose() {}
+    @Override
+    public void show() {}
 }
