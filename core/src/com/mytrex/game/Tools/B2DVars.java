86336137770630a5f27a8f4331b40d605d9df2c3
Antilamer <Antilamer1709@gmail.com>
2015-11-29 02:42:53 +0100

    CAmera

--- core/src/com/mytrex/game/Tools/B2DVars.java
+++ core/src/com/mytrex/game/Tools/B2DVars.java
@@ -1,5 +1,7 @@
 package com.mytrex.game.Tools;
 
+import java.util.ArrayList;
+
 /**
  * Created by Antilamer on 24.11.2015.
  */
@@ -8,4 +10,29 @@ public class B2DVars {
 
     //pixels per meter
     public static final float PPM = 16f;
+
+    public static final ArrayList<Integer> noPassBlocks = new ArrayList<>();
+    static {
+        noPassBlocks.add(136);
+        noPassBlocks.add(137);
+        noPassBlocks.add(138);
+        noPassBlocks.add(691);
+        noPassBlocks.add(692);
+        noPassBlocks.add(693);
+        noPassBlocks.add(694);
+        noPassBlocks.add(718);
+        noPassBlocks.add(719);
+        noPassBlocks.add(720);
+        noPassBlocks.add(721);
+        noPassBlocks.add(666);
+        noPassBlocks.add(667);
+        noPassBlocks.add(639);
+        noPassBlocks.add(640);
+        noPassBlocks.add(1);
+        noPassBlocks.add(207);
+        noPassBlocks.add(208);
+        noPassBlocks.add(140);
+        noPassBlocks.add(190);
+    }
+
 }
--- core/src/com/mytrex/game/scene/TestMapScene.java
+++ core/src/com/mytrex/game/scene/TestMapScene.java
@@ -2,14 +2,11 @@ package com.mytrex.game.scene;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.mytrex.game.GameWorld;
 import com.mytrex.game.PlayerInputProcessor;
@@ -17,6 +14,7 @@ import com.mytrex.game.PlayerInputProcessor;
 import java.util.ArrayList;
 
 import static com.mytrex.game.Tools.B2DVars.PPM;
+import static com.mytrex.game.Tools.B2DVars.noPassBlocks;
 
 
 public class TestMapScene implements Screen {
@@ -29,31 +27,8 @@ public class TestMapScene implements Screen {
     TiledMapTileLayer layer2;
     private Box2DDebugRenderer debuger;
     private GameWorld gameWorld;
-    Label poslabel;
 
     public TestMapScene() {
-        ArrayList<Integer> noPassBlocks = new ArrayList<>();
-        noPassBlocks.add(136);
-        noPassBlocks.add(137);
-        noPassBlocks.add(138);
-        noPassBlocks.add(691);
-        noPassBlocks.add(692);
-        noPassBlocks.add(693);
-        noPassBlocks.add(694);
-        noPassBlocks.add(718);
-        noPassBlocks.add(719);
-        noPassBlocks.add(720);
-        noPassBlocks.add(721);
-        noPassBlocks.add(666);
-        noPassBlocks.add(667);
-        noPassBlocks.add(639);
-        noPassBlocks.add(640);
-        noPassBlocks.add(1);
-        noPassBlocks.add(207);
-        noPassBlocks.add(208);
-        noPassBlocks.add(140);
-        noPassBlocks.add(190);
-
         map = new TmxMapLoader().load("core/assets/map2.tmx");
         camera = new OrthographicCamera(256 / PPM, 16 / PPM);
         camera.setToOrtho(false, 256 / PPM, 256 / PPM);
@@ -102,8 +77,8 @@ public class TestMapScene implements Screen {
                 TiledMapTileLayer.Cell cell2 = layer2.getCell(i, j);
                 try{
                     if (cell2.getTile().getId() == 109){
-                        cell2.getTile().setOffsetX(gameWorld.player.getBody().getPosition().x * PPM);
-                        cell2.getTile().setOffsetY(gameWorld.player.getBody().getPosition().y * PPM);
+                        cell2.getTile().setOffsetX(gameWorld.player.getBody().getPosition().x * PPM - 1.5f * PPM);
+                        cell2.getTile().setOffsetY(gameWorld.player.getBody().getPosition().y * PPM - 1.5f * PPM);
                     }
 
                 }
