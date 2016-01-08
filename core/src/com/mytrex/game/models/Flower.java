db0516a785449442e73b012c20ba40321e59f7dd
Antilamer <Antilamer1709@gmail.com>
2016-01-08 20:22:55 +0200

    flower

--- core/src/com/mytrex/game/models/Flower.java
+++ core/src/com/mytrex/game/models/Flower.java
@@ -3,6 +3,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 
 import static com.mytrex.game.Tools.B2DVars.PPM;
@@ -14,9 +15,12 @@ public class Flower {
     private TextureRegion[] frames;
     private Texture sheet;
     private Animation animation;
+    private long startTime;
+    private long spawnTime;
 
 
     public Flower(Body body) {
+
         batch = new SpriteBatch();
         this.body = body;
         sheet = new Texture("core/assets/flower.png");
@@ -25,9 +29,18 @@ public class Flower {
             frames[i] = new TextureRegion(sheet, 16 * i, 0, 16, 16);
         }
         animation = new Animation(0.2f, frames);
+        startTime = System.currentTimeMillis();
+        spawnTime = 1000l;
     }
 
     public void draw(float stateTime, float x, float y) {
+
+        if (System.currentTimeMillis()<startTime+spawnTime)
+        {
+            //body.getTransform().setPosition(new Vector2(x*2-PPM,(y-(System.currentTimeMillis()-startTime+spawnTime)/1000)* 2 - PPM));
+            body.setTransform(new Vector2(x * 2 - PPM, y - (float)(System.currentTimeMillis() - startTime + spawnTime) /1000 * 2 - PPM), 0);
+            System.out.println((float)(System.currentTimeMillis() - startTime + spawnTime) /1000);
+        }
         batch.begin();
         batch.draw(animation.getKeyFrame(stateTime, true), x * 2 - PPM, y * 2 - PPM, 32, 32);
         batch.end();
